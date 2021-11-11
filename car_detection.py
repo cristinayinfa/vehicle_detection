7f556ee02f0d3ff719290b6da06d2b57f96664e6
cristinayinfa <48993177+cristinayinfa@users.noreply.github.com>
2021-11-11 17:40:16 -0500

    Update car_detection.py

--- car_detection.py
+++ car_detection.py
@@ -1,12 +1,12 @@
 import cv2
 
-c = cv2.VideoCapture('traffic.mp4')
+cap = cv2.VideoCapture('traffic.mp4')
 cc = cv2.CascadeClassifier('cars.xml')
 
 while True:
-    ret, f = c.read()
+    ret, frame = cap.read()
     if not ret:
         break
 
-    if len(cc.detectMultiScale(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY), 1.1, 9)) > 0:
-        print("Vehicle detected at: " + str(c.get(cv2.CAP_PROP_POS_MSEC)) + " ms")
+    if len(cc.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 1.1, 9)) > 0:
+        print("Vehicle detected at: " + str(cap.get(cv2.CAP_PROP_POS_MSEC)) + " ms")
